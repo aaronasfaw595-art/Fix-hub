a9fd8ca3199aae49107336a3d96cfef1b003dfd6
Aron Asfaw <aronasfaw595@gmail.com>
2026-06-23 21:28:48 +0300

    Fix case sensitivity for product route

--- FixHub/backend/routes/productRoutes.js
+++ FixHub/backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Product = require("../models/Product");
+const Product = require("../models/product");
 const Notification = require("../models/notification");
 const multer = require("multer");
 
